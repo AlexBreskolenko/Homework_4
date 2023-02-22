c825ee69b68fe0de1c652e3d1bfa99bd151900b0
Alex <abreskolenko@yandex.ru>
2023-02-22 21:31:23 +0300

    create task 25

---
+++ Task_27/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 27: Напишите метод, который принимает на вход число и выдаёт сумму цифр в числе.
+
+452 -> 11
+
+82 -> 10
+
+9012 -> 12
+*/
+Console.Clear();
+
+int SumNumbers(string num)
+{
+    int result = 0;
+    
+    for(int i = 0; i < num.Length; i++)
+    {
+        result += Convert.ToInt32(num[i] - '0');
+    }
+    
+    return result;
+}
+
+Console.Write("Введите число : ");
+string num = Console.ReadLine();
+
+int result = SumNumbers(num);
+
+Console.WriteLine(result);
